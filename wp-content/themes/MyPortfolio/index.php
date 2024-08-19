c9bf0de4add4db9a7a3deaaeb35625a2a69746a3
Tod9197 <dancein18@yahoo.co.jp>
2024-08-19 23:35:25 +0900

    css,img,jsファイル作成

--- wp-content/themes/MyPortfolio/index.php
+++ wp-content/themes/MyPortfolio/index.php
@@ -1,1 +1,14 @@
-テストテストテストテスト
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo esc_url(get_theme_file_uri('/css/style.css')); ?>" type="text/css">
+  <script src="<?php echo esc_url(get_theme_file_uri('/js/main.js'));?>"></script>
+  <title>My Portfolio</title>
+</head>
+<body>
+  <h1>ポートフォリオ</h1>
+  <img class="horse" src="<?php echo esc_url(get_theme_file_uri('/img/horse-01.jpg')); ?>" alt="">
+</body>
+</html>

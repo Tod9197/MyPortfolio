20e161d8b8e85ebc43f51ba4b00c6c27db7326db
Tod9197 <dancein18@yahoo.co.jp>
2024-08-21 20:20:30 +0900

    実績詳細ページの作成

--- wp-content/themes/MyPortfolio/index.php
+++ wp-content/themes/MyPortfolio/index.php
@@ -1,10 +1,30 @@
 <?php get_header(); ?>
 
-
 <!-- メインビジュアル -->
-<main>
-  
-</main>
+<section class="mainVisual">
+
+</section>
+
+
+<!-- 制作一覧 -->
+<section class="works">
+  <div class="inner">
+  <h2 class="worksTitle">Works</h2>
+  </div>
+</section>
+
+<!-- スキル -->
+<section class="skills">
+  <div class="inner">
+  <h2 class=" skillsTitle">Skills</h2>
+
+  <ul class="skillsList">
+    <li class="skillsList__item">
+      <img src="<?php echo esc_url(get_theme_file_uri('/img/php-icon.png')) ?>" alt="">
+    </li>
+  </ul>
+</div>
+</section>
 
   <?php wp_footer(); ?>
 </body>
